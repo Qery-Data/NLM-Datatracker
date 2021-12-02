079c3f975ee4f7b34d45c6542eb8fca2801980fd
Qery <94573148+Qery-Data@users.noreply.github.com>
2021-12-02 13:46:23 +0100

    Update SSB_ledige_stillinger.py

--- SSB_ledige_stillinger.py
+++ SSB_ledige_stillinger.py
@@ -33,4 +33,34 @@ dataset = pyjstat.Dataset.read(resultat.text)
 type(dataset)
 df = dataset.write('dataframe')
 df.to_csv('data/SSB_ledige_stillinger.csv', index=False)
- 
+ssburl = 'https://data.ssb.no/api/v0/no/table/11587/'
+query = {
+  "query": [
+    {
+      "code": "NACE2007",
+      "selection": {
+        "filter": "vs:NACE2007ledstillNN3",
+        "values": [
+          "01-96"
+        ]
+      }
+    },
+    {
+      "code": "ContentsCode",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "LedigeStillingerPros"
+        ]
+      }
+    }
+  ],
+  "response": {
+    "format": "json-stat2"
+  }
+}
+resultat = requests.post(ssburl, json = query)
+dataset = pyjstat.Dataset.read(resultat.text)
+type(dataset)
+df = dataset.write('dataframe')
+df.to_csv('data/SSB_ledige_stillinger_pst.csv', index=False)
