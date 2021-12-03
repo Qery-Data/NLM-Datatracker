33430710321116747215f4e7d1122df720796728
Qery <94573148+Qery-Data@users.noreply.github.com>
2021-12-03 12:18:25 +0100

    Add sector

--- SSB_ledige_stillinger.py
+++ SSB_ledige_stillinger.py
@@ -64,3 +64,62 @@ dataset = pyjstat.Dataset.read(resultat.text)
 type(dataset)
 df = dataset.write('dataframe')
 df.to_csv('data/SSB_ledige_stillinger_pst.csv', index=False)
+ssburl = 'https://data.ssb.no/api/v0/no/table/11587/'
+query = {
+  "query": [
+    {
+      "code": "NACE2007",
+      "selection": {
+        "filter": "vs:NACE2007ledstillNN3",
+        "values": [
+          "01-03",
+          "05-09",
+          "10-33",
+          "35-39",
+          "41-43",
+          "45-47",
+          "49-53",
+          "55-56",
+          "58-63",
+          "64-66",
+          "68",
+          "69-75",
+          "77-82",
+          "84",
+          "85",
+          "86",
+          "87",
+          "88",
+          "90-93",
+          "94-96"
+        ]
+      }
+    },
+    {
+      "code": "ContentsCode",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "LedigeStillinger"
+        ]
+      }
+    },
+    {
+      "code": "Tid",
+      "selection": {
+        "filter": "top",
+        "values": [
+          "1"
+        ]
+      }
+    }
+  ],
+  "response": {
+    "format": "json-stat2"
+  }
+}
+resultat = requests.post(ssburl, json = query)
+dataset = pyjstat.Dataset.read(resultat.text)
+type(dataset)
+df = dataset.write('dataframe')
+df.to_csv('data/SSB_ledige_stillinger_naring.csv', index=False)
