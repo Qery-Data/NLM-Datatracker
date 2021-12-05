c04b83106d43c5b099daeeb39dc9680d4c47d8bc
Qery <94573148+Qery-Data@users.noreply.github.com>
2021-12-05 08:54:31 +0100

    Update oecd.py

--- oecd.py
+++ oecd.py
@@ -6,5 +6,5 @@ def get_from_oecd(sdmx_query):
 data = (get_from_oecd("LAB_REG_VAC/SWE+DEN+FIN+NOR.LMUNRLTT_STSA.M/"))
 df = pd.DataFrame(data)
 df_new = df.pivot(index='TIME', columns='Country', values='Value')
-df_new2 = df_new.tail(60)
+df_new2 = df_new.tail(12)
 df_new2.to_csv('data/OECDledigestillinger.csv', index=True)
