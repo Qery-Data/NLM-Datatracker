a4b3ee1bf8ca03b0b4d19a0d63c0ae2c680cd768
Qery <94573148+Qery-Data@users.noreply.github.com>
2021-12-06 09:04:35 +0100

    Update SSB_jobber.py

--- SSB_jobber.py
+++ SSB_jobber.py
@@ -1,1 +1,44 @@
-
+from pyjstat import pyjstat
+import requests
+import os
+os.makedirs('data', exist_ok=True)
+ssburl = 'https://data.ssb.no/api/v0/no/table/13126/'
+query = {
+  "query": [
+    {
+      "code": "NACE2007",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "00-99"
+        ]
+      }
+    },
+    {
+      "code": "ForelopigEndelig",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "02"
+        ]
+      }
+    },
+    {
+      "code": "ContentsCode",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "AntArbForholdSesong"
+        ]
+      }
+    }
+  ],
+  "response": {
+    "format": "json-stat2"
+  }
+}
+resultat = requests.post(ssburl, json = query)
+dataset = pyjstat.Dataset.read(resultat.text)
+type(dataset)
+df = dataset.write('dataframe')
+df.to_csv('data/SSB_jobber_totalt.csv', index=False)
