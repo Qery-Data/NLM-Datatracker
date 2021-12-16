ef9c14d54a118422b20a944444599e59951fdbb1
Qery <94573148+Qery-Data@users.noreply.github.com>
2021-12-16 14:52:59 +0100

    test

--- SSB_jobber.py
+++ SSB_jobber.py
@@ -152,8 +152,7 @@ query = {
           "84",
           "85",
           "86-88",
-          "90-99",
-          "00"
+          "90-99"
         ]
       }
     },
@@ -180,7 +179,7 @@ query = {
       "selection": {
         "filter": "top",
         "values": [
-          "2"
+          "62"
         ]
       }
     }
@@ -193,19 +192,34 @@ resultat = requests.post(ssburl, json = query)
 dataset = pyjstat.Dataset.read(resultat.text)
 type(dataset)
 df = dataset.write('dataframe')
-df.to_csv('data/SSB_jobber_naring.csv', index=True)
+df_new = df.pivot(index='næring (SN2007)', columns='måned', values='value')
+df_new2 = df_new.iloc[:,[0,24,48,59,60]]
+antall = df_new2.iloc[:,4]
+tittel_dato = (antall.name)
+antall.name = 'antall'
+Endring_mnd = df_new2.iloc[:,4] - df_new2.iloc[:,3]
+Endring_12 = df_new2.iloc[:,4] - df_new2.iloc[:,2]
+Endring_3 = df_new2.iloc[:,4] - df_new2.iloc[:,1]
+Endring_5 = df_new2.iloc[:,4] - df_new2.iloc[:,0]
+df_new3 = pd.concat([antall, Endring_mnd, Endring_12, Endring_3, Endring_5], axis=1)
+df_new3.to_csv('data/SSB_jobber_naring.csv', index=True)
+date_string = tittel_dato.replace("M","")
+date_string2 = datetime.strptime(date_string, "%Y%m")
+date_string3 = 'Sesongjusterte tall for ' + date_string2.strftime ('%B %Y') +'.'
+locale.setlocale(locale.LC_TIME, 'no')
 json_object = json.loads(resultat.text)
 oppdatert = json_object["updated"]
 oppdatert_dato = datetime.datetime.strptime(oppdatert, '%Y-%m-%dT%H:%M:%SZ')
 riktig_dato = 'Sist publiserte data: ' + oppdatert_dato.strftime ('%d/%m/%y')
 dw = Datawrapper(access_token = os.getenv('DW_TOKEN'))
-dw.refresh_data('5YMDQ')
+dw.refresh_data('S6QM8')
 properties = {
   'annotate' : {
     'notes': riktig_dato,
   }
 }
-dw.update_metadata('5YMDQ', properties)
+dw.update_metadata('S6QM8', properties)
+dw.update_description('S6QM8', intro=date_string3)
 
 ssburl = 'https://data.ssb.no/api/v0/no/table/13126/'
 query = {
