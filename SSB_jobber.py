07cbffed9accf0ef1b40689184f1f064eafd35c1
Qery <94573148+Qery-Data@users.noreply.github.com>
2021-12-16 14:28:16 +0100

    test

--- SSB_jobber.py
+++ SSB_jobber.py
@@ -1,10 +1,10 @@
-from pyjstat import pyjstat
+import pyjstat
 import requests
 import os
 import json
-from datetime import datetime
+import datetime
 import locale
-from datawrapper import Datawrapper
+import Datawrapper
 
 os.makedirs('data', exist_ok=True)
 
