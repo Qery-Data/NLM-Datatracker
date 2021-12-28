e25821dcf8bf6dfc0712d2cf8c00664c6e6a1d5e
Qery <94573148+Qery-Data@users.noreply.github.com>
2021-12-28 10:16:17 +0100

    SSB arbeidsledige integrate

--- SSB_arbeidsledige.py
+++ SSB_arbeidsledige.py
@@ -1,7 +1,15 @@
 from pyjstat import pyjstat
 import requests
 import os
+import json
+from datetime import datetime
+import locale
+import pandas as pd
+locale.setlocale(locale.LC_TIME, 'nb_NO')
 os.makedirs('data', exist_ok=True)
+access_token = os.getenv('DW_TOKEN')
+
+#Antall arbeidsledige oF7tM
 ssburl = 'https://data.ssb.no/api/v0/no/table/13332/'
 query = {
   "query": [
@@ -49,6 +57,21 @@ dataset = pyjstat.Dataset.read(resultat.text)
 type(dataset)
 df = dataset.write('dataframe')
 df.to_csv('data/SSB_arbeidsledige.csv', index=False)
+json_object = json.loads(resultat.text)
+oppdatert = json_object["updated"]
+oppdatert_dato = datetime.strptime(oppdatert, '%Y-%m-%dT%H:%M:%SZ')
+riktig_dato = 'Data sist publisert: ' + oppdatert_dato.strftime ('%d/%m/%y')
+#Update DW
+url = "https://api.datawrapper.de/v3/charts/oF7tM/"
+payload = {"metadata": {"annotate": {"notes": riktig_dato}}}
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*",
+    "Content-Type": "application/json"
+    }
+response = requests.request("PATCH", url, json=payload, headers=headers)
+
+#Andel arbeidsledige bd63e
 ssburl = 'https://data.ssb.no/api/v0/no/table/13332/'
 query = {
   "query": [
@@ -96,3 +119,16 @@ dataset = pyjstat.Dataset.read(resultat.text)
 type(dataset)
 df = dataset.write('dataframe')
 df.to_csv('data/SSB_arbeidsledige_pst.csv', index=False)
+json_object = json.loads(resultat.text)
+oppdatert = json_object["updated"]
+oppdatert_dato = datetime.strptime(oppdatert, '%Y-%m-%dT%H:%M:%SZ')
+riktig_dato = 'Data sist publisert: ' + oppdatert_dato.strftime ('%d/%m/%y')
+#Update DW
+url = "https://api.datawrapper.de/v3/charts/bd63e/"
+payload = {"metadata": {"annotate": {"notes": riktig_dato}}}
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*",
+    "Content-Type": "application/json"
+    }
+response = requests.request("PATCH", url, json=payload, headers=headers)
