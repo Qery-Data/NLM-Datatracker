1d258fa65ff56fca64e5c4f267552806e9198078
Qery <94573148+Qery-Data@users.noreply.github.com>
2021-12-30 07:57:35 +0100

    changed months 60 to 62

--- eurostat.py
+++ eurostat.py
@@ -42,8 +42,8 @@ headers = {
 response = requests.request("PATCH", url, json=payload, headers=headers)
 
 #Andel arbeidsledige
-dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/une_rt_m?s_adj=SA&lastTimePeriod=60&age=TOTAL&unit=PC_ACT&sex=T')
+dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/une_rt_m?s_adj=SA&lastTimePeriod=62&age=TOTAL&unit=PC_ACT&sex=T')
 type(dataset)
 df = dataset.write('dataframe')
 df_new = df.pivot(index='time', columns='geo', values='value')
-df_new.to_csv('data/Eurostatledighet.csv', index=True)
+df_new.to_csv('data/Eurostat_arbeidsledighet.csv', index=True)
