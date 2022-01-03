afbd73fdb74d3ced0d8184af4136d071003e7b85
Qery <94573148+Qery-Data@users.noreply.github.com>
2022-01-03 09:25:52 +0100

    update wNXU5 with DW last updated timestamp

--- eurostat.py
+++ eurostat.py
@@ -41,9 +41,21 @@ headers = {
     }
 response = requests.request("PATCH", url, json=payload, headers=headers)
 
-#Andel arbeidsledige
+#Andel arbeidsledige wNXU5
 dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/une_rt_m?s_adj=SA&lastTimePeriod=62&age=TOTAL&unit=PC_ACT&sex=T')
 type(dataset)
 df = dataset.write('dataframe')
 df_new = df.pivot(index='time', columns='geo', values='value')
 df_new.to_csv('data/Eurostat_arbeidsledighet.csv', index=True)
+oppdatert = dataset["updated"]
+oppdatert_dato = datetime.strptime(oppdatert, '%Y-%m-%d')
+riktig_dato = 'Data sist publisert: ' + oppdatert_dato.strftime ('%d/%m/%y')
+#Update DW
+url = "https://api.datawrapper.de/v3/charts/wNXU5/"
+payload = {"metadata": {"annotate": {"notes": riktig_dato}}}
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*",
+    "Content-Type": "application/json"
+    }
+response = requests.request("PATCH", url, json=payload, headers=headers)
