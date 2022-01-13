ec2d196d438735f04ba1bc9861b8f36ee8537cee
Qery <94573148+Qery-Data@users.noreply.github.com>
2022-01-13 11:01:57 +0100

    new viz 2lEAx

--- SSB_arbeidstid.py
+++ SSB_arbeidstid.py
@@ -66,64 +66,6 @@ headers = {
     }
 response = requests.request("PATCH", url, json=payload, headers=headers)
 
-#Andel heltid deltid kjønn OIl6d
-ssburl = 'https://data.ssb.no/api/v0/no/table/09790/'
-query = {
-  "query": [
-    {
-      "code": "Kjonn",
-      "selection": {
-        "filter": "item",
-        "values": [
-          "1",
-          "2"
-        ]
-      }
-    },
-    {
-      "code": "Yrke",
-      "selection": {
-        "filter": "item",
-        "values": [
-          "0-9"
-        ]
-      }
-    },
-    {
-      "code": "ContentsCode",
-      "selection": {
-        "filter": "item",
-        "values": [
-          "SysselsHeltidProsent",
-          "SysselsDeltidProsent"
-        ]
-      }
-    }
-  ],
-  "response": {
-    "format": "json-stat2"
-  }
-}
-resultat = requests.post(ssburl, json = query)
-dataset = pyjstat.Dataset.read(resultat.text)
-type(dataset)
-df = dataset.write('dataframe')
-df["statistikkvariabel_ny"] = df["kjønn"]+df["statistikkvariabel"]
-df_new = df.pivot(index='statistikkvariabel_ny', columns='år', values='value')
-df_new.to_csv('data/SSB_arbeidstid_heltid_deltid_kjonn.csv', index=True)
-json_object = json.loads(resultat.text)
-oppdatert = json_object["updated"]
-oppdatert_dato = datetime.strptime(oppdatert, '%Y-%m-%dT%H:%M:%SZ')
-riktig_dato = 'Data sist publisert: ' + oppdatert_dato.strftime ('%d/%m/%y')
-#Update DW
-url = "https://api.datawrapper.de/v3/charts/OIl6d/"
-payload = {"metadata": {"annotate": {"notes": riktig_dato}}}
-headers = {
-    "Authorization": ("Bearer " + access_token),
-    "Accept": "*/*",
-    "Content-Type": "application/json"
-    }
-response = requests.request("PATCH", url, json=payload, headers=headers)
 
 #Andel heltid deltid kvinner WvdXZ
 ssburl = 'https://data.ssb.no/api/v0/no/table/09790/'
@@ -231,6 +173,159 @@ oppdatert_dato = datetime.strptime(oppdatert, '%Y-%m-%dT%H:%M:%SZ')
 riktig_dato = 'Data sist publisert: ' + oppdatert_dato.strftime ('%d/%m/%y')
 #Update DW
 url = "https://api.datawrapper.de/v3/charts/PhYrg/"
+payload = {"metadata": {"annotate": {"notes": riktig_dato}}}
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*",
+    "Content-Type": "application/json"
+    }
+response = requests.request("PATCH", url, json=payload, headers=headers)
+
+#Andel yrke heltid deltid 2lEAx
+ssburl = 'https://data.ssb.no/api/v0/no/table/09790/'
+query = {
+  "query": [
+    {
+      "code": "Kjonn",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "0"
+        ]
+      }
+    },
+    {
+      "code": "Yrke",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "0-9",
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "Andre"
+        ]
+      }
+    },
+    {
+      "code": "ContentsCode",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "SysselsHeltidProsent",
+          "SysselsDeltidProsent"
+        ]
+      }
+    },
+    {
+      "code": "Tid",
+      "selection": {
+        "filter": "top",
+        "values": [
+          1
+        ]
+      }
+    }
+  ],
+  "response": {
+    "format": "json-stat2"
+  }
+}
+resultat = requests.post(ssburl, json = query)
+dataset = pyjstat.Dataset.read(resultat.text)
+type(dataset)
+df = dataset.write('dataframe')
+df_new = df.pivot(index='yrke', columns='statistikkvariabel', values='value')
+df_new.to_csv('data/SSB_arbeidstid_heltid_deltid_yrker.csv', index=True)
+json_object = json.loads(resultat.text)
+oppdatert = json_object["updated"]
+oppdatert_dato = datetime.strptime(oppdatert, '%Y-%m-%dT%H:%M:%SZ')
+riktig_dato = 'Data sist publisert: ' + oppdatert_dato.strftime ('%d/%m/%y')
+dato=df.iloc[0,3]
+date_string = 'Tall for ' + dato + '. Prosent av sysselsatte (15-74 år) som jobber:' 
+#Update DW
+url = "https://api.datawrapper.de/v3/charts/2lEAx/"
+payload = {"metadata": {"annotate": {"notes": riktig_dato}}}
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*",
+    "Content-Type": "application/json"
+    }
+response = requests.request("PATCH", url, json=payload, headers=headers)
+url = "https://api.datawrapper.de/v3/charts/2lEAx/"
+payload = {"metadata": {"describe": {"intro": date_string}}}
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*",
+    "Content-Type": "application/json"
+    }
+response = requests.request("PATCH", url, json=payload, headers=headers)
+
+
+
+
+
+
+
+
+#END#
+
+#Andel heltid deltid kjønn OIl6d
+ssburl = 'https://data.ssb.no/api/v0/no/table/09790/'
+query = {
+  "query": [
+    {
+      "code": "Kjonn",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "1",
+          "2"
+        ]
+      }
+    },
+    {
+      "code": "Yrke",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "0-9"
+        ]
+      }
+    },
+    {
+      "code": "ContentsCode",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "SysselsHeltidProsent",
+          "SysselsDeltidProsent"
+        ]
+      }
+    }
+  ],
+  "response": {
+    "format": "json-stat2"
+  }
+}
+resultat = requests.post(ssburl, json = query)
+dataset = pyjstat.Dataset.read(resultat.text)
+type(dataset)
+df = dataset.write('dataframe')
+df["statistikkvariabel_ny"] = df["kjønn"]+df["statistikkvariabel"]
+df_new = df.pivot(index='statistikkvariabel_ny', columns='år', values='value')
+df_new.to_csv('data/SSB_arbeidstid_heltid_deltid_kjonn.csv', index=True)
+json_object = json.loads(resultat.text)
+oppdatert = json_object["updated"]
+oppdatert_dato = datetime.strptime(oppdatert, '%Y-%m-%dT%H:%M:%SZ')
+riktig_dato = 'Data sist publisert: ' + oppdatert_dato.strftime ('%d/%m/%y')
+#Update DW
+url = "https://api.datawrapper.de/v3/charts/OIl6d/"
 payload = {"metadata": {"annotate": {"notes": riktig_dato}}}
 headers = {
     "Authorization": ("Bearer " + access_token),
