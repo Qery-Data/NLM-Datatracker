7f3846c98a69f9641773e092303e3a3ff4dba863
Qery <94573148+Qery-Data@users.noreply.github.com>
2022-01-15 16:54:50 +0100

    new viz d6F9Q

--- oecd.py
+++ oecd.py
@@ -4,28 +4,19 @@ import os
 import json
 from datetime import datetime
 import locale
+import io
 import pandas as pd
 os.makedirs('data', exist_ok=True)
 access_token = os.getenv('DW_TOKEN')
 
-#Ledige stillinger
-def get_from_oecd(sdmx_query):
-    return pd.read_csv(
-        f"https://stats.oecd.org/SDMX-JSON/data/{sdmx_query}?contentType=csv"
-    )
-data = (get_from_oecd("LAB_REG_VAC/SWE+DEN+FIN+NOR.LMUNRLTT_STSA.M/"))
-df = pd.DataFrame(data)
+#Gjennomsnittlig årlig arbeidstimer d6F9Q
+oecd_url='https://stats.oecd.org/SDMX-JSON/data/ANHRS/DNK+NOR+SWE+USA+OECD.TE.A/all?startTime=1970&pid=c0dcdd50-2d08-440b-94d7-8aa50471b7ff'
+resultat = requests.get(oecd_url, headers={'Accept': 'text/csv'})
+df=pd.read_csv(io.StringIO(resultat.text))
 df_new = df.pivot(index='TIME', columns='Country', values='Value')
-df_new2 = df_new.tail(12)
-df_new2.to_csv('data/OECDledigestillinger.csv', index=True)
+df_new.to_csv('data/OECD_arbeidstid_aarligsnitt.csv', index=True)
 
-#Gjennomsnittlig årlig arbeidstimer
-def get_from_oecd(sdmx_query):
-    return pd.read_csv(
-        f"https://stats.oecd.org/SDMX-JSON/data/{sdmx_query}?contentType=csv"
-    )
-data = (get_from_oecd("ANHRS/SWE+DEN+FIN+NOR/"))
-df = pd.DataFrame(data)
-df_new = df.pivot(index='TIME', columns='Country', values='Value')
-df_new2 = df_new.tail(12)
-df_new2.to_csv('data/OECD_arbeidstid_aarligsnitt.csv', index=True)
+
+#END
+
+
