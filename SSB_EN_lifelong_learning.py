3a115e86ddf980546317be6ced66746a57e5efdb
aloenne-qery <alf.lonne@qery.no>
2022-04-16 09:12:40 +0200

    fix locale setting

--- SSB_EN_lifelong_learning.py
+++ SSB_EN_lifelong_learning.py
@@ -5,7 +5,6 @@ import json
 from datetime import datetime
 import locale
 import pandas as pd
-locale.setlocale(locale.LC_TIME, 'nb_NO')
 os.makedirs('data', exist_ok=True)
 access_token = os.getenv('DW_TOKEN')
 
