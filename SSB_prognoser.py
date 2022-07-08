87bb9eb5e77d380bb06078e5f78c6a51e61920b6
aloenne-qery <alf.lonne@qery.no>
2022-07-08 11:49:27 +0200

    sysselsatte personer

--- SSB_prognoser.py
+++ SSB_prognoser.py
@@ -121,7 +121,7 @@ df_new3 = df_new3.transpose()
 df_new3['Dato'] = df_new3.index.map(forecast_dates)
 df_new3.to_csv('data/Prognoser_registrert__tabell.csv', index=True)
 
-#Prognoser Sysselatte personer smBL7 (no)
+#Prognoser Sysselsatte personer
 ssburl = 'https://data.ssb.no/api/v0/no/table/12880/'
 query = {
   "query": [
@@ -165,7 +165,30 @@ df = dataset.write('dataframe')
 df_new = df.pivot(index='år', columns='statistikkvariabel', values='value')
 df_new = df_new.rename(columns={'Sysselsatte personer': 'Faktisk utvikling'})
 df_new ['SSB'] = df_new['Faktisk utvikling']
-df_new.to_csv('data/SSB_prognoser_sysselsattepersoner.csv', index=True)
+df_new.loc[df_new.index[7:11],'Faktisk utvikling'] = pd.NA
+df_new.loc[df_new.index[0:7],'SSB'] = pd.NA
+forecasts = {
+    'Norges Bank': [3.3,0.5,0.2,0.2],
+    'FIN': [3.0,0.8,pd.NA,pd.NA],
+    'NAV': [3.1,0.9,pd.NA,pd.NA],
+    'OECD': [3.0,0.6,pd.NA,pd.NA],
+    'NHO': [2.5,1.1,0.9,pd.NA],
+    'DNB': [2.8,0.5,0.2,0.1],
+    'Danske Bank': [2.7,0.7,pd.NA,pd.NA],
+    'Swedbank': [2.4,0.6,pd.NA,pd.NA]
+    }
+df_forecast = pd.DataFrame(forecasts, index=['2022','2023','2024','2025'])
+df_new2 = pd.concat([df_new, df_forecast], axis=1)
+df_new2['Konsensus'] = df_new2.mean(axis=1)
+years_na = ['2015','2016','2017','2018','2019','2020']
+insert_na = [pd.NA,pd.NA,pd.NA,pd.NA,pd.NA,pd.NA]
+df_new2.loc[years, 'Konsensus'] = insert_na
+df_new2.to_csv('data/Prognoser_sysselsatte_personer_konsensus_figur.csv', index=True)
+df_new3 = df_new2.drop(index={'2015','2016','2017','2018','2019','2020','2021'})
+df_new3.drop(columns=['Faktisk utvikling'], inplace=True)
+df_new3 = df_new3.transpose()
+df_new3['Dato'] = df_new3.index.map(forecast_dates)
+df_new3.to_csv('data/Prognoser_sysselsatte_personer_tabell.csv', index=True)
 
 #Prognoser Arbeidsstyrken aFpl4 (no)
 ssburl = 'https://data.ssb.no/api/v0/no/table/12880/'
