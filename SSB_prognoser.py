040cd7cb8ecaae17201d498bf9b9c6fd56e40586
aloenne-qery <alf.lonne@qery.no>
2022-07-08 12:07:25 +0200

    aarslonn

--- SSB_prognoser.py
+++ SSB_prognoser.py
@@ -315,7 +315,7 @@ df_new3 = df_new3.transpose()
 df_new3['Dato'] = df_new3.index.map(forecast_dates)
 df_new3.to_csv('data/Prognoser_yrkesandel_tabell.csv', index=True)
 
-#Prognoser Utførte timeverk agoxS (no)
+#Prognose Utførte timeverk
 ssburl = 'https://data.ssb.no/api/v0/no/table/12880/'
 query = {
   "query": [
@@ -359,9 +359,16 @@ df = dataset.write('dataframe')
 df_new = df.pivot(index='år', columns='statistikkvariabel', values='value')
 df_new = df_new.rename(columns={'Utførte timeverk i Fastlands-Norge': 'Faktisk utvikling'})
 df_new ['SSB'] = df_new['Faktisk utvikling']
-df_new.to_csv('data/SSB_prognoser_timeverk.csv', index=True)
+df_new.loc[df_new.index[7:11],'Faktisk utvikling'] = pd.NA
+df_new.loc[df_new.index[0:6],'SSB'] = pd.NA
+df_new.to_csv('data/Prognoser_timverk_konsensus_figur.csv', index=True)
+df_new2 = df_new.drop(index={'2015','2016','2017','2018','2019','2020','2021'})
+df_new2.drop(columns=['Faktisk utvikling'], inplace=True)
+df_new2 = df_new2.transpose()
+df_new2['Dato'] = df_new2.index.map(forecast_dates)
+df_new2.to_csv('data/Prognoser_timeverk_tabell.csv', index=True)
 
-#Prognoser Årslønn TZZSL (no)
+#Prognose Årslønn
 ssburl = 'https://data.ssb.no/api/v0/no/table/12880/'
 query = {
   "query": [
@@ -405,7 +412,29 @@ df = dataset.write('dataframe')
 df_new = df.pivot(index='år', columns='statistikkvariabel', values='value')
 df_new = df_new.rename(columns={'Årslønn': 'Faktisk utvikling'})
 df_new ['SSB'] = df_new['Faktisk utvikling']
-df_new.to_csv('data/SSB_prognoser_aarslonn.csv', index=True)
+df_new.loc[df_new.index[7:11],'Faktisk utvikling'] = pd.NA
+df_new.loc[df_new.index[0:7],'SSB'] = pd.NA
+forecasts = {
+    'Norges Bank': [3.9,4.5,4.4,4.2],
+    'Danske Bank': [3.8,3.7,pd.NA,pd.NA],
+    'DNB': [3.9,4.0,4.0,3.5],
+    'Handelsbanken': [4.1,4.0,3.5,pd.NA],
+    'Nordea': [4.5,4.0,pd.NA,pd.NA],
+    'SEB': [4.0,3.6,pd.NA,pd.NA],
+    'Swedbank': [4.1,3.3,pd.NA,pd.NA]
+    }
+df_forecast = pd.DataFrame(forecasts, index=['2022','2023','2024','2025'])
+df_new2 = pd.concat([df_new, df_forecast], axis=1)
+df_new2['Konsensus'] = df_new2.mean(axis=1)
+years_na = ['2015','2016','2017','2018','2019','2020']
+insert_na = [pd.NA,pd.NA,pd.NA,pd.NA,pd.NA,pd.NA]
+df_new2.loc[years_na, 'Konsensus'] = insert_na
+df_new2.to_csv('data/Prognoser_aarslonn_figur.csv', index=True)
+df_new3 = df_new2.drop(index={'2015','2016','2017','2018','2019','2020','2021'})
+df_new3.drop(columns=['Faktisk utvikling'], inplace=True)
+df_new3 = df_new3.transpose()
+df_new3['Dato'] = df_new3.index.map(forecast_dates)
+df_new3.to_csv('data/Prognoser_aarslonn_tabell.csv', index=True)
 
 #Prognoser Konsumprisindeksen sNzun (no)
 ssburl = 'https://data.ssb.no/api/v0/no/table/12880/'
@@ -453,7 +482,7 @@ df_new = df_new.rename(columns={'Konsumprisindeksen (KPI)': 'Faktisk utvikling'}
 df_new ['SSB'] = df_new['Faktisk utvikling']
 df_new.to_csv('data/SSB_prognoser_kpi.csv', index=True)
 
-#BNP Fastlands-Norge
+#Prognose BNP Fastlands-Norge
 ssburl = 'https://data.ssb.no/api/v0/no/table/12880/'
 query = {
   "query": [
@@ -508,8 +537,8 @@ forecasts = {
     'NHO': [3.5,2.2,1.7,pd.NA],
     'Danske Bank': [3.6,1.6,pd.NA,pd.NA],
     'DNB': [3.8,1.5,1.2,1.1],
-    'Handelsbanken': [3.6,1.9,1,pd.NA],
-    'Nordea': [3.5,2,pd.NA,pd.NA],
+    'Handelsbanken': [3.6,1.9,1.0,pd.NA],
+    'Nordea': [3.5,2.0,pd.NA,pd.NA],
     'SEB': [3.7,1.5,pd.NA,pd.NA],
     'Swedbank': [3.2,1.6,pd.NA,pd.NA]
     }
@@ -523,20 +552,5 @@ df_new2.to_csv('data/Prognoser_bnp_fn_konsensus_figur.csv', index=True)
 df_new3 = df_new2.drop(index={'2015','2016','2017','2018','2019','2020','2021'})
 df_new3.drop(columns=['Faktisk utvikling'], inplace=True)
 df_new3 = df_new3.transpose()
-forecast_dates = {
-    'SSB': '10.06.2022',
-    'Norges Bank': '23.06.2022',
-    'FIN': '12.05.2022',
-    'NAV': '15.06.2022',
-    'OECD': '08.06.2022',
-    'IMF': '19.04.2022',
-    'NHO': '15.06.2022',
-    'Danske Bank': '21.06.2022',
-    'DNB': '28.04.2022',
-    'Handelsbanken': '18.05.2022',
-    'Nordea': '11.05.2022',
-    'SEB': '10.05.2022',
-    'Swedbank': '06.04.2022'
-    }
 df_new3['Dato'] = df_new3.index.map(forecast_dates)
 df_new3.to_csv('data/Prognoser_bnp_fn_tabell.csv', index=True)
