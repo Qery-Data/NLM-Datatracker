aeeea3c8243e293ae9cb746f91731c251e45b432
aloenne-qery <alf.lonne@qery.no>
2022-08-17 10:08:11 +0200

    dix

--- SSB_lonn.py
+++ SSB_lonn.py
@@ -1215,7 +1215,7 @@ raw_date = json_object["updated"]
 parsed_date = datetime.strptime(raw_date, '%Y-%m-%dT%H:%M:%SZ')
 chart_date = 'Data sist publisert: ' + parsed_date.strftime ('%d/%m/%y')
 title_date=str(df.iloc[0,6])
-date_string = ' Etter ulike beregningsmåter.' + 'Tall for ' + title_date +'.'
+date_string = ' Etter ulike beregningsmåter.' + ' Tall for ' + title_date +'.'
 #Update DW
 chartid = 'RFHWE'
 url = "https://api.datawrapper.de/v3/charts/" + chartid + '/'
