758dbbc1cdd3db5134221b9faf523af731636b43
aloenne-qery <alf.lonne@qery.no>
2023-03-24 13:05:31 +0100

    updates NB and NAV

--- SSB_prognoser.py
+++ SSB_prognoser.py
@@ -12,9 +12,9 @@ access_token = os.getenv('DW_TOKEN')
 #Prognose datoer
 forecast_dates = {
     'SSB': '10.03.2023',
-    'Norges Bank': '15.12.2022',
+    'Norges Bank': '23.03.2023',
     'FIN': '05.03.2023',
-    'NAV': '06.12.2022',
+    'NAV': '22.03.2023',
     'IMF': '12.10.2022',
     'OECD': '22.11.2022',
     'NHO': '07.03.2023',
@@ -75,7 +75,7 @@ df_new.loc[df_new.index[7:11],'Faktisk utvikling'] = pd.NA
 df_new.loc[df_new.index[0:7],'SSB'] = pd.NA
 forecasts = {
     'FIN': [3.5,3.6,pd.NA,pd.NA],
-    'NAV': [3.7,3.9,pd.NA,pd.NA],
+    'NAV': [3.6,3.8,pd.NA,pd.NA],
     'IMF': [3.8,3.7,pd.NA,pd.NA],
     'OECD': [3.6,3.7,pd.NA,pd.NA],
     'NHO': [3.7,3.8,3.7,pd.NA],
@@ -101,8 +101,8 @@ df_new3.to_csv('data/Prognoser_arbeidsledighet_tabell.csv', index=True)
 df = {'Faktisk utvikling': [3.0,2.7,2.4,2.3,5.0,3.1,1.8,pd.NA,pd.NA,pd.NA,pd.NA]}
 df_new = pd.DataFrame(df, index=['2016','2017','2018','2019','2020','2021','2022','2023','2024','2025','2026'])
 forecasts = {
-    'NAV': [1.9,2.1,pd.NA,pd.NA],
-    'Norges Bank': [2.0,2.4,2.4,pd.NA],
+    'NAV': [1.8,2.1,pd.NA,pd.NA],
+    'Norges Bank': [1.9,2.2,2.3,2.3],
     'FIN': [1.9,2.0,pd.NA,pd.NA],
     'NHO': [2.2,2.4,2.3,pd.NA],
     'DNB': [2.1,2.5,2.9,pd.NA],
@@ -172,9 +172,9 @@ df_new ['SSB'] = df_new['Faktisk utvikling']
 df_new.loc[df_new.index[7:11],'Faktisk utvikling'] = pd.NA
 df_new.loc[df_new.index[0:7],'SSB'] = pd.NA
 forecasts = {
-    'Norges Bank': [-0.4,-0.4,0.7,pd.NA],
+    'Norges Bank': [0.9,-0.1,0.3,0.5],
     'FIN': [0.7,0.2,pd.NA,pd.NA],
-    'NAV': [0.0,0.0,pd.NA,pd.NA],
+    'NAV': [-0.1,0.0,pd.NA,pd.NA],
     'NHO': [0.7,0.6,0.3,pd.NA],
     'LO':[0.0,0.0,-0.2,pd.NA],
     'DNB': [-0.2,0.2,0.2,pd.NA],
@@ -243,7 +243,7 @@ df_new ['SSB'] = df_new['Faktisk utvikling']
 df_new.loc[df_new.index[7:11],'Faktisk utvikling'] = pd.NA
 df_new.loc[df_new.index[0:7],'SSB'] = pd.NA
 forecasts = {
-    'NAV': [0.5,0.5,pd.NA,pd.NA],
+    'NAV': [0.3,0.3,pd.NA,pd.NA],
     }
 df_forecast = pd.DataFrame(forecasts, index=['2023','2024','2025','2026'])
 df_new2 = pd.concat([df_new, df_forecast], axis=1)
@@ -306,7 +306,7 @@ df_new ['SSB'] = df_new['Faktisk utvikling']
 df_new.loc[df_new.index[7:11],'Faktisk utvikling'] = pd.NA
 df_new.loc[df_new.index[0:7],'SSB'] = pd.NA
 forecasts = {
-    'NAV': [72.5,72.3,pd.NA,pd.NA]
+    'NAV': [72.4,72.1,pd.NA,pd.NA]
     }
 df_forecast = pd.DataFrame(forecasts, index=['2023','2024','2025','2026'])
 df_new2 = pd.concat([df_new, df_forecast], axis=1)
@@ -423,7 +423,7 @@ df_new ['SSB'] = df_new['Faktisk utvikling']
 df_new.loc[df_new.index[7:11],'Faktisk utvikling'] = pd.NA
 df_new.loc[df_new.index[0:7],'SSB'] = pd.NA
 forecasts = {
-    'Norges Bank': [4.7,4.3,4.0,pd.NA],
+    'Norges Bank': [5.1,4.4,4.0,3.4],
     'Danske Bank': [4.3,3.8,pd.NA,pd.NA],
     'DNB': [4.8,4.5,4.0,pd.NA],
     'Handelsbanken': [4.6,4.0,pd.NA,pd.NA],
@@ -492,7 +492,7 @@ df_new ['SSB'] = df_new['Faktisk utvikling']
 df_new.loc[df_new.index[7:11],'Faktisk utvikling'] = pd.NA
 df_new.loc[df_new.index[0:7],'SSB'] = pd.NA
 forecasts = {
-    'Norges Bank': [4.8,2.8,2.6,pd.NA],
+    'Norges Bank': [4.9,3.3,2.6,2.1],
     'IMF': [3.8,2.7,pd.NA,pd.NA],
     'OECD': [4.5,3.2,pd.NA,pd.NA],
     'NHO': [4.9,3.3,2.9,pd.NA],
@@ -565,9 +565,9 @@ df_new ['SSB'] = df_new['Faktisk utvikling']
 df_new.loc[df_new.index[7:11],'Faktisk utvikling'] = pd.NA
 df_new.loc[df_new.index[0:7],'SSB'] = pd.NA
 forecasts = {
-    'Norges Bank': [-0.2,0.2,1.4,pd.NA],
+    'Norges Bank': [1.1,0.5,1.1,1.3],
     'FIN': [0.9,1.4,pd.NA,pd.NA],
-    'NAV': [1.2,1.5,pd.NA,pd.NA],
+    'NAV': [1.0,1.4,pd.NA,pd.NA],
     'OECD': [0.7,1.3,pd.NA,pd.NA],
     'IMF': [2.6,2.2,pd.NA,pd.NA],
     'NHO': [1.3,1.4,1.4,pd.NA],
